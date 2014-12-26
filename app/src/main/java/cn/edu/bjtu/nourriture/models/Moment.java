6cfd884ec1f6852fae431a666b871b8665f86019
Pavel <pprochazka72@gmail.com>
2014-12-26 17:28:31 +0800

    Count the elapsed time

--- app/src/main/java/cn/edu/bjtu/nourriture/models/Moment.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/models/Moment.java
@@ -1,8 +1,5 @@
 package cn.edu.bjtu.nourriture.models;
 
-import org.w3c.dom.Comment;
-
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -45,8 +42,8 @@ public class Moment {
         this.momentAuthor = momentAuthor;
     }
 
-    public Date getMomentCreated() {
-        return momentCreated;
+    public String getMomentCreated() {
+        return convertDateToTimeElapsedString(this.momentCreated, new Date());
     }
 
     public void setMomentCreated(Date momentCreated) {
@@ -75,5 +72,61 @@ public class Moment {
 
     public void setMomentSubjectID(String momentSubjectID) {
         this.momentSubjectID = momentSubjectID;
+    }
+
+
+    /**
+     *  Count the elapsed time between now and moment created
+     */
+    public String convertDateToTimeElapsedString(Date startDate, Date endDate){
+
+        String timeElapsed = "";
+
+        //milliseconds
+        long different = endDate.getTime() - startDate.getTime();
+
+        /*System.out.println("startDate : " + startDate);
+        System.out.println("endDate : "+ endDate);
+        System.out.println("different : " + different);*/
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        long elapsedDays = different / daysInMilli;
+        different = different % daysInMilli;
+
+        long elapsedHours = different / hoursInMilli;
+        different = different % hoursInMilli;
+
+        long elapsedMinutes = different / minutesInMilli;
+        different = different % minutesInMilli;
+
+        long elapsedSeconds = different / secondsInMilli;
+
+        if(elapsedDays != 0){
+            if (elapsedDays == 1)
+                timeElapsed += elapsedDays + " day ago";
+            else
+                timeElapsed += elapsedDays + " days ago";
+        }
+        else if (elapsedDays == 0 && elapsedHours != 0 ){
+            if (elapsedHours == 1)
+                timeElapsed += elapsedHours + " hour ago";
+            else
+                timeElapsed += elapsedHours + " hours ago";
+        }
+        else if (elapsedHours == 0 && elapsedMinutes != 0 ){
+            if (elapsedMinutes == 1)
+                timeElapsed += elapsedMinutes + " minute ago";
+            else
+                timeElapsed += elapsedMinutes + " minutes ago";
+        }
+        else if (elapsedMinutes == 0){
+            timeElapsed += "A few seconds ago";
+        }
+
+        return timeElapsed;
     }
 }
