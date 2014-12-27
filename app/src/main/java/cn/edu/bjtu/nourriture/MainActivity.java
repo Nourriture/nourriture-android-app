8d0b52d9ff1aa3c5aac5a8c9d78e75e9be3cfc2b
Pavel <pprochazka72@gmail.com>
2014-12-27 08:25:33 +0800

    Uncommented the use of MAIN and GLOBAL menu

--- app/src/main/java/cn/edu/bjtu/nourriture/MainActivity.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/MainActivity.java
@@ -186,7 +186,9 @@ public class MainActivity extends ActionBarActivity
             // Only show items in the action bar relevant to this screen
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
+
+            //getMenuInflater().inflate(R.menu.main, menu);     TODO: may wanna uncomment in the future to provide this menu everywhere
+
             restoreActionBar(); //update Action bar
             return true;
         }
--- app/src/main/java/cn/edu/bjtu/nourriture/NavigationDrawerFragment.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/NavigationDrawerFragment.java
@@ -160,7 +160,9 @@ public class NavigationDrawerFragment extends Fragment {
         // If the drawer is open, show the global app actions in the action bar. See also
         // showGlobalContextActionBar, which controls the top-left area of the action bar.
         if (mDrawerLayout != null && isDrawerOpen()) {
-            inflater.inflate(R.menu.global, menu);
+
+            //inflater.inflate(R.menu.global, menu);    TODO: may wanna uncomment in the future to provide this menu everywhere
+
             showGlobalContextActionBar();
         }
         super.onCreateOptionsMenu(menu, inflater);
