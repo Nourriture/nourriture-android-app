4f3de0b95e11e304bab5e29adece80357fa279ac
Pavel <pprochazka72@gmail.com>
2015-01-02 18:05:45 +0800

    Generated POJO from JSON

--- app/src/main/java/cn/edu/bjtu/nourriture/models/Consumer.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/models/Consumer.java
@@ -1,168 +1,281 @@
 package cn.edu.bjtu.nourriture.models;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by Pavel Procházka on 26/12/14.
  */
+
+@Generated("org.jsonschema2pojo")
 public class Consumer {
 
     // Constants definition
-    public static final String CONSUMER_USERNAME       = "username";
-    public static final String CONSUMER_NAME           = "name";
-    public static final String CONSUMER_PICTURE        = "picture";
-    public static final String CONSUMER_OCCUPATION     = "occupation";
-    public static final String CONSUMER_BIRTHDATE      = "birthdate";
-    public static final String CONSUMER_WEBSITE        = "website";
-    public static final String CONSUMER_BIO            = "bio";
-    public static final String CONSUMER_EMAIL          = "email";
-    public static final String CONSUMER_GENDER         = "gender";
-    
-    // Properties
-    private String  consumerUsername;    //ID
-    private String  consumerName;
-    private String  consumerPicture;
-    private String  consumerOccupation;
-    private Date    consumerBirthdate;
-    private String  consumerWebsite;
-    private String  consumerBio;
-    private String  consumerEmail;      //not used anywhere yet
-    private int     consumerGender;     // 0 = not assigned, 1 = woman, 2 = man
+    public static final String CONSUMER_USERNAME = "username";
+    public static final String CONSUMER_NAME = "name";
+    public static final String CONSUMER_PICTURE = "picture";
+    public static final String CONSUMER_OCCUPATION = "occupation";
+    public static final String CONSUMER_BIRTHDATE = "birthdate";
+    public static final String CONSUMER_WEBSITE = "website";
+    public static final String CONSUMER_BIO = "bio";
+    public static final String CONSUMER_EMAIL = "email";
+    public static final String CONSUMER_GENDER = "gender";
+
+    @SerializedName("_id")
+    @Expose
+    private String Id;
+    @Expose
+    private String created;
+    @Expose
+    private String username;
+    @Expose
+    private String email;
+    @Expose
+    private String name;
+    @Expose
+    private String picture;
+    @Expose
+    private String bio;
+    @Expose
+    private String occupation;
+    @Expose
+    private Integer gender;
+    @Expose
+    private String birthday;
+    @Expose
+    private String website;
+    @Expose
+    private List<Object> following = new ArrayList<Object>();
+
     private ArrayList<HashMap> consumerInfoToDisplay = null;   //consumer may provide different amount of information, therefore we return only the provided one
 
-    public String getConsumerWebsite() {
-        return consumerWebsite;
-    }
-
-    public void setConsumerWebsite(String consumerWebsite) {
-        this.consumerWebsite = consumerWebsite;
-    }
-
-    public String getConsumerUsername() {
-        return consumerUsername;
-    }
-
-    public void setConsumerUsername(String consumerUsername) {
-        this.consumerUsername = consumerUsername;
-    }
-
-    public String getConsumerName() {
-        return consumerName;
-    }
-
-    public void setConsumerName(String consumerName) {
-        this.consumerName = consumerName;
-    }
-
-    public String getConsumerEmail() {
-        return consumerEmail;
-    }
-
-    public void setConsumerEmail(String consumerEmail) {
-        this.consumerEmail = consumerEmail;
-    }
-
-    public String getConsumerPicture() {
-        return consumerPicture;
-    }
-
-    public void setConsumerPicture(String consumerPicture) {
-        this.consumerPicture = consumerPicture;
-    }
-
-    public String getConsumerOccupation() {
-        return consumerOccupation;
-    }
-
-    public void setConsumerOccupation(String consumerOccupation) {
-        this.consumerOccupation = consumerOccupation;
-    }
-
-    public String getConsumerBio() {
-        return consumerBio;
-    }
-
-    public void setConsumerBio(String consumerBio) {
-        this.consumerBio = consumerBio;
-    }
-
-    public int getConsumerGender() {
-        return consumerGender;
-    }
-
-    public void setConsumerGender(int consumerGender) {
-        this.consumerGender = consumerGender;
-    }
-
-    public Date getConsumerBirthdate() {
-        return consumerBirthdate;
-    }
-
-    public void setConsumerBirthdate(Date consumerBirthdate) {
-        this.consumerBirthdate = consumerBirthdate;
+    /**
+     * @return The Id
+     */
+    public String getId() {
+        return Id;
     }
 
     /**
-     *
-     * @return array list of hashmaps -> each consisting of attribute name and its value
+     * @param Id The _id
      */
-   public ArrayList<HashMap> getConsumerInfoToDisplay(){
-
-       if (consumerInfoToDisplay == null){
-           consumerInfoToDisplay = findAvailableInfo();
-       }
-
-       return consumerInfoToDisplay;
+    public void setId(String Id) {
+        this.Id = Id;
     }
 
     /**
-     * Sets the order of items!!!
-     * @return
+     * @return The created
      */
-    private ArrayList findAvailableInfo(){
+    public String getCreated() {
+        return created;
+    }
+
+    /**
+     * @param created The created
+     */
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    /**
+     * @return The username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username The username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return The email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email The email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return The picture
+     */
+    public String getPicture() {
+        return picture;
+    }
+
+    /**
+     * @param picture The picture
+     */
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    /**
+     * @return The bio
+     */
+    public String getBio() {
+        return bio;
+    }
+
+    /**
+     * @param bio The bio
+     */
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    /**
+     * @return The occupation
+     */
+    public String getOccupation() {
+        return occupation;
+    }
+
+    /**
+     * @param occupation The occupation
+     */
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    /**
+     * @return The gender
+     */
+    public Integer getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender The gender
+     */
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return The birthday
+     */
+    public String getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * @param birthday The birthday
+     */
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * @return The website
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * @param website The website
+     */
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    /**
+     * @return The following
+     */
+    public List<Object> getFollowing() {
+        return following;
+    }
+
+    /**
+     * @param following The following
+     */
+    public void setFollowing(List<Object> following) {
+        this.following = following;
+    }
+
+
+    public ArrayList<HashMap> getConsumerInfoToDisplay() {
+
+        if (consumerInfoToDisplay == null) {
+            consumerInfoToDisplay = findAvailableInfo();
+        }
+
+        return consumerInfoToDisplay;
+    }
+
+
+    private ArrayList findAvailableInfo() {
         ArrayList<HashMap> result = new ArrayList<>();
 
-        if (this.consumerPicture != null && !this.consumerPicture.isEmpty()){
+        if (picture != null && !picture.isEmpty()) {
             HashMap picDic = new HashMap();
-            picDic.put(Consumer.CONSUMER_PICTURE, this.consumerPicture);
+            picDic.put(Consumer.CONSUMER_PICTURE, picture);
             result.add(picDic);
         }
 
-        if (this.consumerUsername != null && !this.consumerUsername.isEmpty()){
+        if (username != null && !username.isEmpty()) {
             HashMap usernameDic = new HashMap();
-            usernameDic.put(Consumer.CONSUMER_USERNAME, this.consumerUsername);
+            usernameDic.put(Consumer.CONSUMER_USERNAME, username);
             result.add(usernameDic);
         }
 
-        if (this.consumerName != null && !this.consumerName.isEmpty()){
+        if (name != null && !name.isEmpty()) {
             HashMap nameDic = new HashMap();
-            nameDic.put(Consumer.CONSUMER_NAME, this.consumerName);
+            nameDic.put(Consumer.CONSUMER_NAME, name);
             result.add(nameDic);
         }
 
-        if (this.consumerOccupation != null && !this.consumerOccupation.isEmpty()){
+        if (occupation != null && !occupation.isEmpty()) {
             HashMap occupationDic = new HashMap();
-            occupationDic.put(Consumer.CONSUMER_OCCUPATION, this.consumerOccupation);
+            occupationDic.put(Consumer.CONSUMER_OCCUPATION, occupation);
             result.add(occupationDic);
         }
 
-        if (this.consumerBirthdate != null && this.consumerBirthdate != null){
+        /*if (birthday != null && birthday != null) {
             HashMap birthdateDic = new HashMap();
-            birthdateDic.put(Consumer.CONSUMER_BIRTHDATE, this.consumerBirthdate.toString());
+            birthdateDic.put(Consumer.CONSUMER_BIRTHDATE, birthday.toString());
             result.add(birthdateDic);
-        }
+        }*/
 
-        if (this.consumerWebsite != null && !this.consumerWebsite.isEmpty()){
+        if (website != null && !website.isEmpty()) {
             HashMap websiteDic = new HashMap();
-            websiteDic.put(Consumer.CONSUMER_WEBSITE, this.consumerWebsite);
+            websiteDic.put(Consumer.CONSUMER_WEBSITE, website);
             result.add(websiteDic);
         }
 
-        if (this.consumerBio != null && !this.consumerBio.isEmpty()){
+        if (bio != null && !bio.isEmpty()) {
             HashMap bioDic = new HashMap();
-            bioDic.put(Consumer.CONSUMER_BIO, this.consumerBio);
+            bioDic.put(Consumer.CONSUMER_BIO, bio);
             result.add(bioDic);
         }
 
