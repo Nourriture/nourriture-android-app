d63b6ab6d86324b725b8ebd7015908a4a001b531
Pavel <pprochazka72@gmail.com>
2015-01-03 23:04:23 +0800

    Receiving the Moment and activity title from Intent

--- app/src/main/java/cn/edu/bjtu/nourriture/activities/SocialListOfMomentActivity.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/activities/SocialListOfMomentActivity.java
@@ -1,18 +1,47 @@
 package cn.edu.bjtu.nourriture.activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import cn.edu.bjtu.nourriture.R;
+import cn.edu.bjtu.nourriture.models.Moment;
+
+enum LIST_TYPE{
+    LIST_TYPE_LIKES,
+    LIST_TYPE_COMMENTS
+}
 
 public class SocialListOfMomentActivity extends ActionBarActivity {
 
+
+
+    // --- PROPERTIES ---
+    private LIST_TYPE   listViewType;
+    private Moment      momentToShowDetails;
+
+
+
+    // --- ACTIVITY lifecycle methods ---
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_list_of_moment);
+
+        // Get the moment and activity title from the intent
+        Intent intent = getIntent();    //get the Intent that started your activity by calling getIntent() and retrieve the data contained within the intent
+        momentToShowDetails = (Moment) intent.getSerializableExtra(DetailMomentActivity.DETAILED_MOMENT);
+
+        //TODO: set title for this Activity
+        String title = intent.getStringExtra(DetailMomentActivity.DETAILED_MOMENT_ACTIVITY_TITLE);
+        if (title.equals(getString(R.string.listTypeLikes))){
+            listViewType = LIST_TYPE.LIST_TYPE_LIKES;
+        }
+        else if (title.equals(getString(R.string.listTypeComments))){
+            listViewType = LIST_TYPE.LIST_TYPE_COMMENTS;
+        }
     }
 
 
