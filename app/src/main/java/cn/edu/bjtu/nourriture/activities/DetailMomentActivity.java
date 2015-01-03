b47ace1233c39ef27bc87f712952cb8f6fa5aa4b
Pavel <pprochazka72@gmail.com>
2015-01-03 12:11:52 +0800

    Custom adapter

--- app/src/main/java/cn/edu/bjtu/nourriture/activities/DetailMomentActivity.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/activities/DetailMomentActivity.java
@@ -3,12 +3,21 @@ package cn.edu.bjtu.nourriture.activities;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import cn.edu.bjtu.nourriture.R;
 import cn.edu.bjtu.nourriture.models.Moment;
@@ -27,17 +36,32 @@ public class DetailMomentActivity extends ActionBarActivity {
     // --- PROPERTIES ---
     private String current_moment_id;
 
+    private MomentAdapter adapter;
+
+    private ArrayList<HashMap> momentDataToShow;
+
+    private ListView listView;
+
+
 
     // --- ACTIVITY lifecycle methods ---
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_detail_moment);
+
         // Get the moment id from the intent
         Intent intent = getIntent();    //get the Intent that started your activity by calling getIntent() and retrieve the data contained within the intent
         current_moment_id = intent.getStringExtra(MainActivity.DETAILED_MOMENT_ID);
 
-        setContentView(R.layout.activity_detail_moment);
+        momentDataToShow = new ArrayList<>();
+
+        adapter = new MomentAdapter();
+
+        // can findViewById, because View already populated by setContentView
+        listView = (ListView) findViewById(R.id.momentDetailsListView);
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -96,7 +120,11 @@ public class DetailMomentActivity extends ActionBarActivity {
         api.getMoment(current_moment_id, new Callback<Moment>() {
             @Override
             public void success(Moment moment, Response response) {
+                momentDataToShow.clear();
 
+                momentDataToShow.addAll(moment.getMomentInfoToDisplay());
+
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -117,5 +145,83 @@ public class DetailMomentActivity extends ActionBarActivity {
         //TODO: implement
         Toast toast = Toast.makeText(getApplicationContext(), "Feature not implemented yet", Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+
+
+    // --- CUSTOM INNER CLASS of ArrayAdapter ---
+    private class MomentAdapter extends ArrayAdapter {
+
+        // takes CONTEXT, LAYOUT and DATA
+        public MomentAdapter() {
+            super(getBaseContext(), R.layout.row_consumer_title_and_value, momentDataToShow);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+
+            View rowView = null;
+
+            rowView = inflater.inflate(R.layout.row_consumer_title_and_value, parent, false);
+
+            TextView titleTextView = (TextView) rowView.findViewById(R.id.titleTextView);
+            TextView valueTextView = (TextView) rowView.findViewById(R.id.valueTextView);
+
+            titleTextView.setText("Title");
+            valueTextView.setText("Value");
+
+            /*HashMap consumerInfo = consumerDataToShow.get(position);
+
+            // row with IMAGE
+            if (consumerInfo.containsKey(Consumer.CONSUMER_PICTURE)){
+                rowView = inflater.inflate(R.layout.row_consumer_image, parent, false);
+
+                ImageView imgView = (ImageView) rowView.findViewById(R.id.consumerImageView);
+
+                String url = consumerInfo.get(Consumer.CONSUMER_PICTURE).toString();
+                Picasso.with(getContext())
+                        .load(url)
+                        .resize(160, 160)
+                        .centerCrop()
+                        .placeholder(R.drawable.user_placeholder)
+                        .into(imgView);
+            }
+            // row with TITLE and VALUE
+            else {
+                rowView = inflater.inflate(R.layout.row_consumer_title_and_value, parent, false);
+
+                TextView titleTextView = (TextView) rowView.findViewById(R.id.titleTextView);
+                TextView valueTextView = (TextView) rowView.findViewById(R.id.valueTextView);
+
+                if (consumerInfo.containsKey(Consumer.CONSUMER_USERNAME)){
+                    titleTextView.setText(R.string.consumerUsername);
+                    valueTextView.setText(consumerInfo.get(Consumer.CONSUMER_USERNAME).toString());
+                }
+                else if (consumerInfo.containsKey(Consumer.CONSUMER_NAME)){
+                    titleTextView.setText(R.string.consumerName);
+                    valueTextView.setText(consumerInfo.get(Consumer.CONSUMER_NAME).toString());
+                }
+                else if (consumerInfo.containsKey(Consumer.CONSUMER_OCCUPATION)){
+                    titleTextView.setText(R.string.consumerOccupation);
+                    valueTextView.setText(consumerInfo.get(Consumer.CONSUMER_OCCUPATION).toString());
+                }
+                else if (consumerInfo.containsKey(Consumer.CONSUMER_BIRTHDATE)){
+                    titleTextView.setText(R.string.consumerBirthdate);
+                    valueTextView.setText(consumerInfo.get(Consumer.CONSUMER_BIRTHDATE).toString());
+                }
+                else if (consumerInfo.containsKey(Consumer.CONSUMER_WEBSITE)){
+                    titleTextView.setText(R.string.consumerWebsite);
+                    valueTextView.setText(consumerInfo.get(Consumer.CONSUMER_WEBSITE).toString());
+                }
+                else if (consumerInfo.containsKey(Consumer.CONSUMER_BIO)){
+                    titleTextView.setText(R.string.consumerBio);
+                    valueTextView.setText(consumerInfo.get(Consumer.CONSUMER_BIO).toString());
+                }
+            }*/
+
+            return rowView;
+        }
     }
 }
