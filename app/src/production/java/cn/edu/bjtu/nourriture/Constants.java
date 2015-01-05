f1e4f5729d5e5d1a8b325a5961805ad2c507cff3
Pavel <pprochazka72@gmail.com>
2015-01-06 03:08:51 +0800

    new Constants file

---
+++ app/src/production/java/cn/edu/bjtu/nourriture/Constants.java
@@ -0,0 +1,9 @@
+package cn.edu.bjtu.nourriture.services;
+
+/**
+ * Created by Pavel Procházka on 31/12/14.
+ */
+public interface Constants {
+    public String NOURRITURE_PLATFORM_URL           = "http://9la.dk/platform";
+    public String NOURRITURE_PLATFORM_ANDROID_URL   = "http://9la.dk/android_platform"; //FIXME: verify correctness of URL!
+}
