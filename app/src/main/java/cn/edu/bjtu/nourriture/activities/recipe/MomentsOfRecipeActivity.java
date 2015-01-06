894fb2eb65a538d7c29e52f55c9e96585d99bd1c
Pavel <pprochazka72@gmail.com>
2015-01-07 03:27:18 +0800

    Open up new moment activity when required

--- app/src/main/java/cn/edu/bjtu/nourriture/activities/recipe/MomentsOfRecipeActivity.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/activities/recipe/MomentsOfRecipeActivity.java
@@ -1,14 +1,23 @@
 package cn.edu.bjtu.nourriture.activities.recipe;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import cn.edu.bjtu.nourriture.R;
+import cn.edu.bjtu.nourriture.activities.MainActivity;
+import cn.edu.bjtu.nourriture.activities.moment.NewMomentActivity;
 import cn.edu.bjtu.nourriture.fragments.moments.MomentsFragment;
 
 public class MomentsOfRecipeActivity extends ActionBarActivity implements MomentsFragment.OnFragmentInteractionListener {
+
+
+
+    // --- PROPERTIES ---
+    private String recipeID;
 
 
 
@@ -19,11 +28,18 @@ public class MomentsOfRecipeActivity extends ActionBarActivity implements Moment
 
         setContentView(R.layout.activity_moments_of_recipe);
 
-        String query = getIntent().getStringExtra(DetailRecipeActivity.INTENT_RECIPE_ID);
+        //coming from the DetailRecipeActivity, which passes intentExtra
+        if (getIntent().getStringExtra(DetailRecipeActivity.INTENT_RECIPE_ID) != null){
+            recipeID = getIntent().getStringExtra(DetailRecipeActivity.INTENT_RECIPE_ID);
+        }
+        else {  //coming from the NewMomentActivity, which cannot pass anything (got here by pressing UP button) so have to read from SharedPreferences
+            SharedPreferences pref = getSharedPreferences(MainActivity.SHARED_PREFERENCES_RECIPE_DETAIL, MODE_PRIVATE);
+            recipeID = pref.getString(MainActivity.CURRENT_RECIPE_ID, "");
+        }
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, MomentsFragment.newInstance(101, MomentsFragment.MOMENTS_QUERY_TYPE.RECIPE, query))
+                    .add(R.id.container, MomentsFragment.newInstance(101, MomentsFragment.MOMENTS_QUERY_TYPE.RECIPE, recipeID))
                     .commit();
         }
     }
@@ -31,14 +47,14 @@ public class MomentsOfRecipeActivity extends ActionBarActivity implements Moment
 
 
     // --- ACTION BAR ---
-    @Override
+    // Will get overwritten by the MomentsFragment anyways
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_moments_of_recipe, menu);
         return true;
     }
 
-    // will get overwritten by the MomentsFragment anyways
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -52,7 +68,7 @@ public class MomentsOfRecipeActivity extends ActionBarActivity implements Moment
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
 
 
@@ -60,10 +76,16 @@ public class MomentsOfRecipeActivity extends ActionBarActivity implements Moment
     @Override
     public void onMomentSelected(String id) {
         System.out.println(id);
+        //TODO: probably should show moment detail...
     }
 
     @Override
     public void onNewMomentSelected() {
-        System.out.println("foo");
+
+        // Present the "New Moment" activity modaly (slide up)
+        Intent intent_info = new Intent(MomentsOfRecipeActivity.this, NewMomentActivity.class);
+        intent_info.putExtra(DetailRecipeActivity.INTENT_RECIPE_ID, recipeID);
+        startActivity(intent_info);
+        overridePendingTransition(R.anim.slide_up_animation,R.anim.no_change_animation);
     }
 }
