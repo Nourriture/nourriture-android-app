a435d09b7471b72cb2e1f6caa0a993435694299d
Pavel <pprochazka72@gmail.com>
2015-01-06 20:01:02 +0800

    Major changes (API itegration)

--- app/src/main/java/cn/edu/bjtu/nourriture/activities/recipe/DetailRecipeActivity.java
+++ app/src/main/java/cn/edu/bjtu/nourriture/activities/recipe/DetailRecipeActivity.java
@@ -1,21 +1,118 @@
 package cn.edu.bjtu.nourriture.activities.recipe;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import cn.edu.bjtu.nourriture.R;
+import cn.edu.bjtu.nourriture.activities.MainActivity;
+import cn.edu.bjtu.nourriture.models.Recipe;
+import cn.edu.bjtu.nourriture.services.Constants;
+import cn.edu.bjtu.nourriture.services.NourritureAPI;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.converter.GsonConverter;
 
-public class DetailRecipeActivity extends ActionBarActivity {
+public class DetailRecipeActivity extends ActionBarActivity implements AdapterView.OnItemClickListener{
 
+
+
+    // --- PROPERTIES ---
+    private String current_recipe_id;   //used for API call
+
+    private RecipeAdapter adapter;
+
+    private ArrayList<HashMap> currentRecipeDataToShow; //data source for our adapter
+
+    private Recipe currentRecipe;   //data source used for social list of moments
+
+    private ListView listView;
+
+
+
+    // --- ACTIVITY lifecycle methods ---
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_recipe);
+
+        SharedPreferences pref = getSharedPreferences(MainActivity.MY_RECIPE_DETAIL_PREFERENCES, MODE_PRIVATE); // 0 - for private mode
+        current_recipe_id = pref.getString(MainActivity.MY_RECIPE_ID, "");
+
+        currentRecipeDataToShow = new ArrayList<>();
+
+        currentRecipe = null;
+
+        adapter = new RecipeAdapter();
+
+        // can findViewById, because View already populated by setContentView
+        listView = (ListView) findViewById(R.id.recipeDetailsListView);
+        listView.setAdapter(adapter);
+
+        /**
+         * Register a callback to be invoked when an item in this AdapterView has
+         * been clicked.
+         *
+         * Set OnItemClickListener so we can be notified on item clicks
+         */
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Always fetch recipe when comes to the foreground
+        fetchRecipeDetails();
     }
 
 
+
+    // --- AdapterView.OnItemClickListener
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        /*HashMap item = currentMomentDataToShow.get(position);
+
+        // User wants to see likes
+        if (item.containsKey(Moment.MOMENT_LIKES) && !currentMoment.getLikes().isEmpty()){
+
+            // Present the social list activity with "Likes"
+            Intent intent = new Intent(DetailMomentActivity.this, SocialListOfMomentActivity.class);
+            intent.putExtra(DETAILED_MOMENT_ACTIVITY_TITLE,  getString(R.string.listTypeLikes));
+            intent.putExtra(DETAILED_MOMENT, currentMoment);
+            startActivity(intent);
+        }
+        // User wants to see comments
+        else if (item.containsKey(Moment.MOMENT_COMMENTS) && !currentMoment.getComments().isEmpty()){
+
+            // Present the social list activity with "Comments"
+            Intent intent = new Intent(DetailMomentActivity.this, SocialListOfMomentActivity.class);
+            intent.putExtra(DETAILED_MOMENT_ACTIVITY_TITLE,  getString(R.string.listTypeComments));
+            intent.putExtra(DETAILED_MOMENT, currentMoment);
+            startActivity(intent);
+        }*/
+    }
+
+
+
+    // --- ACTION BAR ---
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -36,5 +133,97 @@ public class DetailRecipeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    // --- API calls ---
+    private void fetchRecipeDetails() {
+        // custom GSON parser http://stackoverflow.com/questions/18473011/retrofit-gson-serialize-date-from-json-string-into-java-util-date
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                .create();
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(Constants.NOURRITURE_PLATFORM_URL)
+                .setConverter(new GsonConverter(gson))
+                .build();
+
+        NourritureAPI api = restAdapter.create(NourritureAPI.class);
+        api.getRecipe(current_recipe_id, new Callback<Recipe>() {
+            @Override
+            public void success(Recipe recipe, Response response) {
+                currentRecipeDataToShow.clear();
+
+                currentRecipeDataToShow.addAll(recipe.getRecipeInfoToDisplay());
+
+                currentRecipe = recipe;
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast toast = Toast.makeText(getApplicationContext(), R.string.api_error, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+
+
+    // --- CUSTOM INNER CLASS of ArrayAdapter ---
+    private class RecipeAdapter extends ArrayAdapter {
+
+        // takes CONTEXT, LAYOUT and DATA
+        public RecipeAdapter() {
+            super(getBaseContext(), R.layout.row_consumer_title_and_value, currentRecipeDataToShow);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+
+            View rowView = null;
+
+            HashMap momentInfo = currentRecipeDataToShow.get(position);
+
+            // row with MOMENT content
+            /*if (momentInfo.containsKey(Moment.MOMENT_TEXT)){
+                rowView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+                TextView textView = (TextView) rowView.findViewById(android.R.id.text1);
+                textView.setText(momentInfo.get(Moment.MOMENT_TEXT).toString());
+                textView.setTextSize(20);
+                //textView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+            }
+            // row with TITLE and VALUE
+            else {
+                rowView = inflater.inflate(R.layout.row_consumer_title_and_value, parent, false);
+
+                TextView titleTextView = (TextView) rowView.findViewById(R.id.titleTextView);
+                TextView valueTextView = (TextView) rowView.findViewById(R.id.valueTextView);
+
+                if (momentInfo.containsKey(Moment.MOMENT_AUTHOR)){
+                    titleTextView.setText(R.string.momentDetailAuthor);
+                    valueTextView.setText(momentInfo.get(Moment.MOMENT_AUTHOR).toString());
+                }
+                else if (momentInfo.containsKey(Moment.MOMENT_CREATED)){
+                    titleTextView.setText(R.string.momentDetailCreated);
+                    valueTextView.setText(momentInfo.get(Moment.MOMENT_CREATED).toString());
+                }
+                else if (momentInfo.containsKey(Moment.MOMENT_LIKES)){
+                    titleTextView.setText(R.string.momentDetailLikes);
+                    valueTextView.setText(momentInfo.get(Moment.MOMENT_LIKES).toString());
+                }
+                else if (momentInfo.containsKey(Moment.MOMENT_COMMENTS)){
+                    titleTextView.setText(R.string.momentDetailComments);
+                    valueTextView.setText(momentInfo.get(Moment.MOMENT_COMMENTS).toString());
+                }
+            }*/
+
+            return rowView;
+        }
     }
 }
