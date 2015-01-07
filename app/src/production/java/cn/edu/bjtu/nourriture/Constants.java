b5ecd9d8460508c857a12c25cb586abdc56e6fb2
Pavel <pprochazka72@gmail.com>
2015-01-07 20:55:11 +0800

    Added LOCALHOST environment

--- app/src/production/java/cn/edu/bjtu/nourriture/Constants.java
+++ app/src/production/java/cn/edu/bjtu/nourriture/Constants.java
@@ -1,9 +1,9 @@
-package cn.edu.bjtu.nourriture.services;
+package cn.edu.bjtu.nourriture;
 
 /**
  * Created by Pavel Procházka on 31/12/14.
  */
 public interface Constants {
     public String NOURRITURE_PLATFORM_URL           = "http://9la.dk/platform";
-    public String NOURRITURE_PLATFORM_ANDROID_URL   = "http://9la.dk/android_platform"; //FIXME: verify correctness of URL!
+    public String NOURRITURE_PLATFORM_ANDROID_URL   = "http://9la.dk/android/";
 }
